d3f821beb7dbaea9dbdbc2e2a71817c5636930e0
Yoda-Flash <kaitlyn.zee@gmail.com>
2026-01-13 18:16:42 -0800

    Imports fixed

--- frc-2026/src/main/java/frc/robot/subsystems/ShooterTestJig.java
+++ frc-2026/src/main/java/frc/robot/subsystems/ShooterTestJig.java
@@ -4,13 +4,12 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
